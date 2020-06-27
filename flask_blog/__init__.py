d9cc0f9856174d58df420c6ba48a4942a35a02f3
deguchi <deguchi.k.110@gmail.com>
2020-06-27 22:45:39 +0900

    dbのimportを後からできるように変更

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+def init_view():
+    from flask_blog.views import views, entries
+
+
 app = Flask(__name__)
 app.config.from_object('flask_blog.config')
 
 db = SQLAlchemy(app)
 
-from flask_blog.views import views, entries
+init_view()
