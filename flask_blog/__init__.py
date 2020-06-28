c51924d89d1e57be2dc1694711cab9ffe19f654d
deguchi <deguchi.k.110@gmail.com>
2020-06-28 14:37:18 +0900

    add Blueprint

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-def init_view():
-    from flask_blog.views import views, entries
-
-
 app = Flask(__name__)
 app.config.from_object('flask_blog.config')
 
 db = SQLAlchemy(app)
 
-init_view()
+from flask_blog.views.views import view
+app.register_blueprint(view)
+
+from flask_blog.views.entries import entry
+app.register_blueprint(entry, url_prefix='/users')
+
+from flask_blog.views import views
